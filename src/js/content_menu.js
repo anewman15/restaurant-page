f0f9371895ff4478b7d17328dd8062646317472f
Abdullah Numan <anewman15@hotmail.com>
2020-12-04 21:00:29 +0600

    Add menu item image using Webpack loader

--- src/js/content_menu.js
+++ src/js/content_menu.js
@@ -1,4 +1,5 @@
 import { resetContentWrapper } from './tabbed_navigation';
+import logo from '../images/logo.png';
 
 const displayContentMenu = () => {
 	resetContentWrapper();
@@ -19,8 +20,8 @@ const displayContentMenu = () => {
 																	<p>1 cold drink</p>
 																	<p>Price: 480 Tk</p>
 																</div>
-																<div class="menu-image">
-																	<img src="../src/images/logo.png">
+																<div>
+																	<img class="menu-image" alt="logo">
 																</div>
 															</div>
 															<hr>
@@ -32,13 +33,18 @@ const displayContentMenu = () => {
 																	<p>1 cold drink</p>
 																	<p>Price: 320 Tk</p>
 																</div>
-																<div class="menu-image">
-																<img src="../src/images/logo.png">
+																<div>
+																<img class="menu-image" alt="logo">
 																</div>
 															</div>
 														</div>
-                          `;
+													`;
+
 	contentWrapper.appendChild(contentMenu);
+	const menuImagesArray = contentMenu.querySelectorAll('.menu-image');
+	menuImagesArray.forEach((menuImage) => {
+		menuImage.src = logo;
+	});
 };
 
 export default displayContentMenu;
